72e97f87b813ae928381bc3abab3039f449af88e
Janne <jannemieskolainen@gmail.com>
2021-09-29 12:47:16 +0300

    basic footer ja muita muutoksia

---
+++ js/registration.js
@@ -0,0 +1,6 @@
+document.getElementById("buttonLog").addEventListener("click", notice)
+document.getElementById("buttonReg").addEventListener("click", notice)
+
+function notice() {
+    console.log("Toimii")
+}
