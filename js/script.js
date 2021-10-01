db9fccfa6666f67054eecb24d481fbaf2d210111
JojoSnow <milzwow94@gmail.com>
2021-10-01 09:55:32 +0300

    base for carousel done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const listingArray = [];
 let objectIndex = 0;
 let listingIndex = 0;
+let slideIndex = 0;
 
 const loginBtn = document.getElementById('login-btn');
 const regBtn = document.getElementById('regis-btn')
@@ -48,6 +49,20 @@ function clickOutsideReg(event) {
     if (event.target === regModal) {
         regModal.style.display = 'none';
     }
+}
+
+//sliding for info images
+function showSlides() {
+    let slides = document.querySelectorAll('.slides');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 7000);
 }
 
 // creating object of the listing
@@ -221,3 +236,4 @@ function expandListing(event) {
 createListingObject(objectIndex);
 createListingObject(objectIndex);
 createListing(listingIndex);
+showSlides();
