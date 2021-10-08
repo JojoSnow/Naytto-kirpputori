cbbbc1b8c3b32cbf7691126a7f227fea08d9454f
Janne <jannemieskolainen@gmail.com>
2021-10-08 09:17:05 +0300

    slides file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let userIndex = 0;
-let slideIndex = 0;
 
 const loginBtn = document.getElementById('login-btn');
 const regBtn = document.getElementById('regis-btn');
@@ -185,19 +184,3 @@ function regSuccess() {
     // closes the registration modal after 2 seconds of a successful registration
     setTimeout(closeReg, 5000);
 }
-
-// sliding for info images -- have a better animation
-function showSlides() {
-    let slides = document.querySelectorAll('.slides');
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = 'block';
-    setTimeout(showSlides, 7000);
-}
-
-showSlides();
